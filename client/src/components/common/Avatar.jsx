2e9fd215b2d83d57382beadf40f734e1da3ffa65
Joshua030 <Joseluy123>
2023-08-16 20:24:46 +0200

    feat, create a newe component ContextMenu.jsx

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
 import { useState } from "react";
-import {FaCamera} from "react-icons/fa"
+import { FaCamera } from "react-icons/fa";
 
 function Avatar({ type, image, setImage }) {
   const [hover, setHover] = useState(false);
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+  const [contextMenuCoordinates, setContextMenuCoordinates] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setContextMenuCoordinates({ x: e.pageX, y: e.pageY });
+    setIsContextMenuVisible(true);
+  };
+
   return (
     <>
       <div className="flex items-center justify-center">
@@ -25,9 +37,22 @@ function Avatar({ type, image, setImage }) {
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
           >
-            <div className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2 ${hover?"visible" : "hidden" }`}>
-              <FaCamera  className="text-2xl" id="context-opener"/>
-              <span>Change Profile Photo</span>
+            <div
+              className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2 ${
+                hover ? "visible" : "hidden"
+              }`}
+              onClick={(e) => showContextMenu(e)}
+              id="context-opener"
+            >
+              <FaCamera
+                className="text-2xl"
+                id="context-opener"
+                onClick={(e) => showContextMenu(e)}
+              />
+              <span id="context-opener" onClick={(e) => showContextMenu(e)}>
+                Change <br />
+                Profile <br /> Photo
+              </span>
             </div>
             <div className="flex items-center h-60 w-60 cursor-pointer">
               <Image src={image} alt="avatar" className="rounded-full" fill />
